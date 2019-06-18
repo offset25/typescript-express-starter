67aa3b38286f230295acc5895da8891790bfa6d3
offset25 <dino.bartolome@gmail.com>
2019-06-17 23:34:08 -0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+starter files for typescript express
+
+npm init
+npm i -g typescript
+npm i express
+
+npm i -D typescript ts-node nodemon @types/node @types/express
+
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+//const greeting: string = 'Hello';
+
+//const numbers: number[] = [1,2,3];
+
+
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+	res.send('Hello');
+});
+
+app.listen(5000, () => console.log('Server running'));
